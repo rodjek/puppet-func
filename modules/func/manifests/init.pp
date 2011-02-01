7b734c5dea24a4ddfbbb5e9492559664d84e80b3
Steve Huff <shuff@vecna.org>
2011-02-01 09:11:13 -0500

    fleshed out the $certmaster_service conditional a bit more

--- modules/func/manifests/init.pp
+++ modules/func/manifests/init.pp
@@ -2,3 +2,17 @@
 Exec {
     path    => ["/sbin", "/usr/sbin", "/bin", "/usr/bin"],
 }
+
+# the certmaster service has a different name under RHEL vs. Debian
+case $operatingsystem {
+    /(Debian|Ubuntu/: { 
+        $certmaster_service = "certmasterd" 
+    },
+    /(RedHat|CentOS|Fedora)/: { 
+        $certmaster_service = "certmaster" 
+    },
+    default: { 
+        $certmaster_service = "certmaster" 
+    },
+}
+
